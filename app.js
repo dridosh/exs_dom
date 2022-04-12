64bbfef2bba53204399f460d18f5b08eef007748
D.Ridosh <dridosh@gmail.com>
2022-04-12 13:37:16 +0400

    first task ready

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// 1. head
+
+// 2. body
+
+// 3. все дочерние элементы body и вывести их в консоль.
+
+// 4. первый div и все его дочерние узлы
+
+// а) вывести все дочерние узлы в консоль
+
+// б) вывести в консоль все дочерние узлы, кроме первого и последнего
+
+
 //1
 console.log(document.head);
 //2
@@ -9,6 +22,42 @@ console.log(document.body.firstElementChild);
 console.log(document.body.firstElementChild.children);
 // 4.б
 const div = document.body.firstElementChild;
-// При помощи оператора ... мы получаем из коллекции массив который фильтруем сравнивая каждый элемент с первым и последним элементом в div. 
+
+// При помощи оператора ... получаем из коллекции массив который фильтруем сравнивая каждый элемент с первым и последним элементом в div. 
 const filteredArticles = [...div.children].filter(p => p !== div.firstElementChild && p !== div.lastElementChild);
-console.log(filteredArticles);
+console.lo
+
+
+// функция, которая принимает два элемента. 
+// Функция проверяет, является ли первый элемент родителем для второго:
+//Функция принимает только DOM объекты. 
+
+function isParent(parent, child) {
+  // Проверяем что переданные элементы являются HTML элементами
+  if (!(parent instanceof HTMLElement) || !(child instanceof HTMLElement)) return;
+
+  let isParent = false;
+  let currentParent = child.parentElement;
+  
+  while(currentParent) {
+    isParent = currentParent === parent
+    if (isParent) {
+      break;
+    }
+
+    currentParent = currentParent.parentElement;
+  }
+
+  return isParent;
+}
+
+isParent(document.body.children[0], document.querySelector('mark'));
+
+
+console.log(isParent(document.body.children[0], document.querySelector('a')));
+
+
+
+
+
+
